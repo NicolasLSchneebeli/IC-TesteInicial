8f9e0421c8563f511554515fbf55a1b60a362129
Nicolas Schneebeli <118636061+NicolasLSchneebeli@users.noreply.github.com>
2023-07-11 16:57:53 -0300

    Comecei a separar dados brutos

--- rasunho.py
+++ rasunho.py
@@ -6,69 +6,83 @@ import seaborn as sns
 import scipy.signal as sp
 from scipy.interpolate import griddata
 
-locD4=np.zeros((7,2))
-FD4=np.zeros(7)
-tapeteposD4=tapetepos.copy()
+locA1ml=np.zeros((7,2))
+FA1ml=np.zeros(7)
+tapeteposA1ml=tapetepos.copy()
 
 for i in range (0,tapetepos.shape[0]):
     for j in range (0,tapetepos.shape[1]):
         if tapetepos[i][j]==1:
-            tapeteposD4[i][j]=(deltalambdaD4[0])*DadosVazioKs["K1"]#N
-            locD4[0]=([i,j-1])
-            FD4[0]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=(deltalambdaA1ml[0])*DadosVazioKs["K1"]#N
+            locA1ml[0]=([i,j-1])
+            FA1ml[0]=tapeteposA1ml[i][j]
             
         if tapetepos[i][j]==2:
-            tapeteposD4[i][j]=(deltalambdaD4[1])*DadosVazioKs["K2"] #N
-            locD4[1]=([i,j])
-            FD4[1]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=(deltalambdaA1ml[1])*DadosVazioKs["K2"] #N
+            locA1ml[1]=([i,j])
+            FA1ml[1]=tapeteposA1ml[i][j]
 
         if tapetepos[i][j]==3:
-            tapeteposD4[i][j]=(deltalambdaD4[2])*DadosVazioKs["K3"] #N
-            locD4[2]=([i,j-1])
-            FD4[2]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=(deltalambdaA1ml[2])*DadosVazioKs["K3"] #N
+            locA1ml[2]=([i,j-1])
+            FA1ml[2]=tapeteposA1ml[i][j]
 
         if tapetepos[i][j]==5:
-            tapeteposD4[i][j]=((deltalambdaD4[3])*DadosVazioKs["K5"]) #N
-            locD4[3]=([i,j-1])
-            FD4[3]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=((deltalambdaA1ml[3])*DadosVazioKs["K5"]) #N
+            locA1ml[3]=([i,j-1])
+            FA1ml[3]=tapeteposA1ml[i][j]
 
         if tapetepos[i][j]==6:  
-            tapeteposD4[i][j]=((deltalambdaD4[4])*DadosVazioKs["K6"]) #N
-            locD4[4]=([i,j-1])
-            FD4[4]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=((deltalambdaA1ml[4])*DadosVazioKs["K6"]) #N
+            locA1ml[4]=([i,j-1])
+            FA1ml[4]=tapeteposA1ml[i][j]
             
         if tapetepos[i][j]==7:
-            tapeteposD4[i][j]=((deltalambdaD4[5])*DadosVazioKs["K7"]) #N
-            locD4[5]=([i,j-1])
-            FD4[5]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=((deltalambdaA1ml[5])*DadosVazioKs["K7"]) #N
+            locA1ml[5]=([i,j-1])
+            FA1ml[5]=tapeteposA1ml[i][j]
             
         if tapetepos[i][j]==8:
-            tapeteposD4[i][j]=(deltalambdaD4[6])*DadosVazioKs["K8"] #N
-            locD4[6]=([i,j-1])
-            FD4[6]=tapeteposD4[i][j]
+            tapeteposA1ml[i][j]=(deltalambdaA1ml[6])*DadosVazioKs["K8"] #N
+            locA1ml[6]=([i,j-1])
+            FA1ml[6]=tapeteposA1ml[i][j]
             
-interp_forcasD4= griddata(locD4,FD4,(xx,yy))
+interp_forcasA1ml= griddata(locA1ml,FA1ml,(xx,yy))
 
 
-im1=plt.imshow(interp_forcasD4 , cmap="hot")
+im1=plt.imshow(interp_forcasA1ml , cmap="hot")
 
 plt.colorbar(im1)
 plt.xlabel('x')
 plt.ylabel('y')
 
 
-deltalambdaD4=np.zeros(7)
+deltalambdaA1ml=np.zeros(7)
 
-deltalambdaD4[0]= medias[0] - Bequer1000pos3['PEAKS1'].mean()
-deltalambdaD4[1]= medias[1] - Bequer1000pos3['PEAKS2'].mean()
-deltalambdaD4[2]= medias[2] - Bequer1000pos3['PEAKS3'].mean()
+deltalambdaA1ml[0]= medias[0] - BequerVaziopos1['PEAKS1']
+deltalambdaA1ml[1]= medias[1] - BequerVaziopos1['PEAKS2']
+deltalambdaA1ml[2]= medias[2] - BequerVaziopos1['PEAKS3']
 
-deltalambdaD4[3]= medias[3] - Bequer1000pos3['PEAKS5'].mean()
-deltalambdaD4[4]= medias[4] - Bequer1000pos3['PEAKS6'].mean()
-deltalambdaD4[5]= medias[5] - Bequer1000pos3['PEAKS7'].mean()
-deltalambdaD4[6]= medias[6] - Bequer1000pos3['PEAKS8'].mean()
+deltalambdaA1ml[3]= medias[3] - BequerVaziopos1['PEAKS5']
+deltalambdaA1ml[4]= medias[4] - BequerVaziopos1['PEAKS6']
+deltalambdaA1ml[5]= medias[5] - BequerVaziopos1['PEAKS7']
+deltalambdaA1ml[6]= medias[6] - BequerVaziopos1['PEAKS8']
 
-Bequer1000pos3=pd.read_csv(open("Teste1/Béquer 1000mL/N0.2023.05.11.16.18.56(pos3).txt","r"),skiprows=65,names=["Timestamp","CH1","CH2","CH3","CH4","CH5","CH6","CH7","CH8","PEAKS1","PEAKS2","PEAKS3","PEAKS5","PEAKS6","PEAKS7","PEAKS8"], sep="\t", decimal=",")
-Bequer1000pos3=pd.read_csv(open("Teste1/Béquer 1000mL/N0.2023.05.11.16.19.37(pos3).txt","r"),skiprows=65,names=["Timestamp","CH1","CH2","CH3","CH4","CH5","CH6","CH7","CH8","PEAKS1","PEAKS2","PEAKS3","PEAKS5","PEAKS6","PEAKS7","PEAKS8"], sep="\t", decimal=",")
-Bequer1000pos3=pd.read_csv(open("Teste1/Béquer 1000mL/N0.2023.05.11.16.20.06(Pos3).txt","r"),skiprows=65,names=["Timestamp","CH1","CH2","CH3","CH4","CH5","CH6","CH7","CH8","PEAKS1","PEAKS2","PEAKS3","PEAKS5","PEAKS6","PEAKS7","PEAKS8"], sep="\t", decimal=",")
-Bequer1000pos4
+BequerVaziopos1=pd.read_csv(open("Teste1/Béquer 1000mL/N0.2023.05.11.16.18.56(pos4).txt","r"),skiprows=65,names=["Timestamp","CH1","CH2","CH3","CH4","CH5","CH6","CH7","CH8","PEAKS1","PEAKS2","PEAKS3","PEAKS5","PEAKS6","PEAKS7","PEAKS8"], sep="\t", decimal=",")
+BequerVaziopos1=pd.read_csv(open("Teste1/Béquer 1000mL/N0.2023.05.11.16.19.37(pos4).txt","r"),skiprows=65,names=["Timestamp","CH1","CH2","CH3","CH4","CH5","CH6","CH7","CH8","PEAKS1","PEAKS2","PEAKS3","PEAKS5","PEAKS6","PEAKS7","PEAKS8"], sep="\t", decimal=",")
+BequerVaziopos1=pd.read_csv(open("Teste1/Béquer 1000mL/N0.2023.05.11.16.20.06(pos4).txt","r"),skiprows=65,names=["Timestamp","CH1","CH2","CH3","CH4","CH5","CH6","CH7","CH8","PEAKS1","PEAKS2","PEAKS3","PEAKS5","PEAKS6","PEAKS7","PEAKS8"], sep="\t", decimal=",")
+
+
+FBpoS2ML= np.zeros([len(BequerVaziopos1),8])  
+FBpoS2ML=pd.DataFrame(FBpoS2ML,columns=["FORCE1",'FORCE2','FORCE3','FORCE4','FORCE5','FORCE6','FORCE7','FORCE8'])
+for i in range (0,len(BequerVaziopos1)):
+    FBpoS2ML['PEAKS1']=(BequerVaziopos1['PEAKS1'][i] - medias[0])*DadosVazioKs['K1']
+    FBpoS2ML['PEAKS2']=(BequerVaziopos1['PEAKS2'][i] - medias[1])*DadosVazioKs['K2']
+    FBpoS2ML['PEAKS3']=(BequerVaziopos1['PEAKS3'][i] - medias[2])*DadosVazioKs['K3']
+    # FBpoS2ML['PEAKS4']=BequerVaziopos1['PEAKS4'][i] #CASO ADICONE MAIS UM SENSOR ADICIONE O VALOR PADRÂO DELE EM MEDIAS    
+    FBpoS2ML['PEAKS5']=(BequerVaziopos1['PEAKS5'][i] - medias[3])*DadosVazioKs['K5']
+    FBpoS2ML['PEAKS6']=(BequerVaziopos1['PEAKS6'][i] - medias[4])*DadosVazioKs['K6']
+    FBpoS2ML['PEAKS7']=(BequerVaziopos1['PEAKS7'][i] - medias[5])*DadosVazioKs['K7']
+    FBpoS2ML['PEAKS8']=(BequerVaziopos1['PEAKS8'][i] - medias[6])*DadosVazioKs['K8']
+    
+FBpoS2ML.to_csv('Teste1/DADOS BRUTOS DE FORÇA/FBpoS2ML.csv')
